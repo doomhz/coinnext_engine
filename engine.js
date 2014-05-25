a7b4b7eaa13be951cb71ae892772d31135bfe945
Dumitru Glavan <dumitru.gl@gmail.com>
2014-05-25 17:42:48 +0200

    Switch queue to one event to process only

--- engine.js
+++ engine.js
@@ -13,73 +13,74 @@ GLOBAL.queue = require('./lib/queue/index');
 var OrderBook = require("./lib/order_book")
 
 var processEvents = function () {
-  processNextCancellation(function () {
-    processNextAdd(function () {
-      processNextMatch(function () {
+  GLOBAL.queue.Event.findNextValid(function (err, event) {
+    if (err) {
+      return console.error("Could not fetch the next event. Exitting...", err);
+    }
+    if (!event) {
+      OrderBook.matchBuyOrders(function () {
         setTimeout(processEvents, QUEUE_DELAY);
       });
-    });
+    } else if (event.type === "cancel_order") {
+      processCancellation(event, function () {
+        setTimeout(processEvents, QUEUE_DELAY);
+      });
+    } else if (event.type === "add_order") {
+      processAdd(event, function () {
+        setTimeout(processEvents, QUEUE_DELAY);
+      });
+    }
   });
 };
 
-var processNextCancellation = function (callback) {
-  GLOBAL.queue.Event.findNext("cancel_order", function (err, event) {
-    if (!event) return callback();
-    var orderId = event.loadout.order_id;
-    OrderBook.deleteOpenOrder(orderId, function (err) {
-      if (!err) {
-        event.status = "processed";
-        event.save().complete(function (err) {
-          if (err) {
-            console.error("Could send event " + event.id, err);
+var processCancellation = function (event, callback) {
+  var orderId = event.loadout.order_id;
+  OrderBook.deleteOpenOrder(orderId, function (err) {
+    if (!err) {
+      event.status = "processed";
+      event.save().complete(function (err) {
+        if (err) {
+          console.error("Could send event " + event.id, err);
+          return callback();
+        } else {
+          GLOBAL.queue.Event.addOrderCanceled({order_id: orderId}, function (err) {
+            if (err) {
+              console.error("Could not add order_cancel for event " + event.id + " order " + orderId, err);
+            }
             return callback();
-          } else {
-            GLOBAL.queue.Event.addOrderCanceled({order_id: orderId}, function (err) {
-              if (err) {
-                console.error("Could not add order_cancel for event " + event.id + " order " + orderId, err);
-              }
-              return callback();
-            });
-          }
-        });
-      } else {
-        console.error("Could not process event " + event.id, err);
-        return callback();
-      }
-    });
+          });
+        }
+      });
+    } else {
+      console.error("Could not process event " + event.id, err);
+      return callback();
+    }
   });
 };
 
-var processNextAdd = function (callback) {
-  GLOBAL.queue.Event.findNext("add_order", function (err, event) {
-    if (!event) return callback();
-    var data = event.loadout;
-    OrderBook.addOrder(data, function (err, order) {
-      if (!err) {
-        event.status = "processed";
-        event.save().complete(function (err) {
-          if (err) {
-            console.error("Could send event " + event.id, err);
+var processAdd = function (event, callback) {
+  var data = event.loadout;
+  OrderBook.addOrder(data, function (err, order) {
+    if (!err) {
+      event.status = "processed";
+      event.save().complete(function (err) {
+        if (err) {
+          console.error("Could send event " + event.id, err);
+          return callback();
+        } else {
+          GLOBAL.queue.Event.addOrderAdded({order_id: order.external_order_id}, function (err) {
+            if (err) {
+              console.error("Could not add order_added for event " + event.id + " order " + order.id, err);
+            }
             return callback();
-          } else {
-            GLOBAL.queue.Event.addOrderAdded({order_id: order.external_order_id}, function (err) {
-              if (err) {
-                console.error("Could not add order_added for event " + event.id + " order " + order.id, err);
-              }
-              return callback();
-            });
-          }
-        });
-      } else {
-        console.error("Could not process event " + event.id, err);
-        return callback();
-      }
-    });
+          });
+        }
+      });
+    } else {
+      console.error("Could not process event " + event.id, err);
+      return callback();
+    }
   });
-};
-
-var processNextMatch = function (callback) {
-  OrderBook.matchBuyOrders(callback);
 };
 
 processEvents();
